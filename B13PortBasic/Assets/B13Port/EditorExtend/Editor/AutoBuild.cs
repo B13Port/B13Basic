4cdfd78bf7395d9cdcea2f981aa2dc2f7b7b2f77
White <bai0613@foxmail.com>
2022-04-12 17:02:38 +0800

    add autobuild scripts,1.0.1

---
+++ B13PortBasic/Assets/B13Port/EditorExtend/Editor/AutoBuild.cs
@@ -0,0 +1,99 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  ~  File：AutoBuild.cs                          
+  ~  Author：B13Port                         
+  ~  E-mail：bai0613@foxmail.com                    
+  ~  Date：2022/04/12 15:59:39                             
+  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+using System;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+using System.IO;
+
+namespace B13Port.EditorExtend
+{
+    public class AutoBuild : EditorWindow
+    {
+
+        static string keystorePass = "123456";
+        static string keyaliasPass = "123456";
+
+        [MenuItem("B13Port/BuildAPK", false, 1)]
+        public static void BuildAPK()
+        {
+            string path = "";
+            EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Android, BuildTarget.Android);
+            PlayerSettings.Android.keystorePass = keystorePass;
+            PlayerSettings.Android.keyaliasPass = keyaliasPass;
+
+            AssetDatabase.Refresh();
+            EditorUserBuildSettings.exportAsGoogleAndroidProject = false;
+            BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions
+            {
+                locationPathName = BundleName("apk", ref path),
+                targetGroup = BuildTargetGroup.Android,
+                target = BuildTarget.Android,
+                options = BuildOptions.None
+            };
+
+            BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+            BuildSummary summary = report.summary;
+
+            if (summary.result == BuildResult.Succeeded)
+            {
+                XDebug.Log($"build android/apk successed:{summary.totalSize / 1024 / 1024} M");
+                System.Diagnostics.Process.Start(path);
+            }
+            else if (summary.result == BuildResult.Failed)
+            {
+                XDebug.LogError("build android/apk failed");
+            }
+        }
+
+        [MenuItem("B13Port/BuildAAB", false, 1)]
+        public static void BuildAAB()
+        {
+            string path = "";
+            EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Android, BuildTarget.Android);
+            PlayerSettings.Android.keystorePass = keystorePass;
+            PlayerSettings.Android.keyaliasPass = keyaliasPass;
+
+            AssetDatabase.Refresh();
+            EditorUserBuildSettings.exportAsGoogleAndroidProject = false;
+            EditorUserBuildSettings.buildAppBundle = true;
+            BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions
+            {
+                locationPathName = BundleName("aab", ref path),
+                targetGroup = BuildTargetGroup.Android,
+                target = BuildTarget.Android,
+                options = BuildOptions.None
+            };
+
+            BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+            BuildSummary summary = report.summary;
+
+            if (summary.result == BuildResult.Succeeded)
+            {
+                XDebug.Log($"build android/aab successed:{summary.totalSize / 1024 / 1024} M");
+                System.Diagnostics.Process.Start(path);
+            }
+            else if (summary.result == BuildResult.Failed)
+            {
+                XDebug.LogError("build android/aab failed");
+            }
+        }
+
+        public static string BundleName(string suffixName, ref string bundlepath)
+        {
+            string path = Application.dataPath;
+            bundlepath = path.Replace("Assets", "APKs");
+            if (!Directory.Exists(bundlepath))
+                Directory.CreateDirectory(bundlepath);
+            string name = Application.productName;
+            string version = Application.version;
+            string time = DateTime.Now.ToString("MM_dd_HH_mm");
+            return $"{bundlepath}/{name}_{version}_{time}.{suffixName}";
+        }
+    }
+}
